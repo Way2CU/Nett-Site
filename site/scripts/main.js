47f5617c83b36b556de5f27d877c1c079f463a40
iliyaM <iliya@way2cu.com>
2016-11-20 15:27:41 +0200

    changed page control tager to section and div.mobile div.desktop

--- site/scripts/main.js
+++ site/scripts/main.js
@@ -71,14 +71,14 @@ Site.on_load = function() {
 	//event listener and function call
 	window.addEventListener('scroll', Site.scroll);
 
-	//Page controll for animation of Show_case gallery tablet and div.content with description and img
-	Site.show_case_gallery_tablet = new PageControl('div.tablet', 'div.content');
+	//Page controll for animation of Show_case gallery desktop and img with description and img
+	Site.show_case_gallery_tablet = new PageControl('section.showcase', 'div.desktop');
 	Site.show_case_gallery_tablet
 		.setInterval(3000)
 		.setWrapAround(true);
 
 	//Page controll for animation of Show_case gallery mobile
-	Site.show_case_gallery_mobile = new PageControl('div.mobile', 'img');
+	Site.show_case_gallery_mobile = new PageControl('section.showcase', 'div.mobile');
 	Site.show_case_gallery_mobile
 		.setInterval(3000)
 		.setWrapAround(true);
