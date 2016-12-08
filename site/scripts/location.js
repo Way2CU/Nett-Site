12902165c468ac652c2edd2a786bca1eb280b8a3
iliyaM <iliya@way2cu.com>
2016-12-08 10:13:17 +0200

    change api key for google maps, location.js has been updated to simpler map function that takes cooarinates

--- site/scripts/location.js
+++ site/scripts/location.js
@@ -1,45 +1,22 @@
 // create or use existing site scope
-var Site = Site || new Object();
+var Site = Site || {};
 
-Site.Map = function() {
-  var self = this;
-
-  self.maps = null;
-  self.geocoder = null;
-
-  self._init = function() {
-    self.maps = new google.maps.Map(document.getElementById('map'),
-      {zoom: 17, mapTypeControl: false, draggable: false, scrollwheel: false});
-    self.geocoder = new google.maps.Geocoder();
-
-    var address = document.querySelector('a.location').innerHTML;
-    self._find_coordinates(address);
-  };
-
-  self._find_coordinates = function(address) {
-    var data = {
-        address: address
-      };
-
-    self.geocoder.geocode(data, self._handle_geocode);
+Site.map = function (langitude, latitude) {
+  var mapCanvas = document.getElementById('map');
+  var mapOptions = {
+    center: {lat: langitude, lng: latitude},
+    zoom: 17
   }
 
-  self._handle_geocode = function(results, status) {
-    if (status !== google.maps.GeocoderStatus.OK)
-      return;
+  var map = new google.maps.Map(mapCanvas, mapOptions);
 
-    self.maps.setCenter(results[0].geometry.location);
-    var data = {
-        map: self.maps,
-        position: results[0].geometry.location
-      };
-    var marker = new google.maps.Marker(data);
-  };
-
-  // finalize object
-  self._init();
+  var marker = new google.maps.Marker({
+    position:{lat: langitude, lng: latitude},
+    map: map,
+    title: "DukiveNapo Shop",
+  });
 }
 
-$(function() {
-    Site.map = new Site.Map();
+$(function(){
+  Site.location = new Site.map(31.991105, 34.771661);
 });
